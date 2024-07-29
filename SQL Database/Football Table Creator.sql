ae871f7f47c6f86bf8b64b42dd1f921980bfe9b5
Wmurry <45493992+Wmurry@users.noreply.github.com>
2024-07-29 05:06:42 -0400

    Add files via upload

---
+++ SQL Database/Football Table Creator.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Persons (
+    ID int NOT NULL PRIMARY KEY,
+    City nvarchar(50),
+	Team_Name nvarchar(50),
+    QB nchar(10),
+	RB nchar(10),
+	WR nchar(10),
+	TE nchar(10),
+	OL nchar(10),
+	DL nchar(10),
+	LB nchar(10),
+	CB nchar(10),
+	S nchar(10),
+	K nchar(10),
+	Coach nchar(10),
+	Momentum nchar(10),
+    Timezone int
+);
